31d540d80a1233a1da5a9ba3683bb8c41949b038
@Snick55 <snikerss123321@gmail.com>
2023-12-25 21:55:39 +0300

    Update

--- app/src/main/java/com/snick55/testcomposeapp/MainActivity.kt
+++ app/src/main/java/com/snick55/testcomposeapp/MainActivity.kt
@@ -1,17 +1,39 @@
 package com.snick55.testcomposeapp
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
 import com.snick55.testcomposeapp.ui.theme.TestComposeAppTheme
 
@@ -29,22 +51,110 @@ class MainActivity : ComponentActivity() {
 }
 
 
-
 @Composable
-fun MyApp(modifier: Modifier = Modifier){
-    Surface(modifier = modifier, color = MaterialTheme.colorScheme.background) {
-        Greeting(name = "Android")
+fun MyApp(
+    modifier: Modifier = Modifier
+) {
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+    } else {
+        Greetings()
     }
 }
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    TestComposeAppTheme {
+        MyApp(modifier = Modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = List(1000) { "$it" }
+) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
+
+    }
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier.padding(24.dp)
-        )
-    }
+    var isExpended by rememberSaveable { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        if (isExpended) 48.dp else 0.dp,
+        label = "test label",
+        animationSpec = spring(dampingRatio =Spring.DampingRatioHighBouncy, stiffness = Spring.StiffnessLow))
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+            ) {
+                Text(text = name, style =MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                ))
+                if (isExpended){
+                    Text(text = stringResource(id = R.string.long_text))
+                }
+            }
 
+            IconButton(onClick = { isExpended = !isExpended }) {
+                Icon(
+                    imageVector = if(isExpended) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = null
+                )
+
+            }
+        }
+    }
 }
 
+@Composable
+fun OnboardingScreen(modifier: Modifier = Modifier, onContinueClicked: () -> Unit) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to start screen!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = { onContinueClicked.invoke() }
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    TestComposeAppTheme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "GreetingPreviewDark"
+)
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun GreetingPreview() {
+    TestComposeAppTheme {
+        Greetings()
+    }
+}
